66e97bf4c132f4c238ed9d183de13293c10e5aff
Lin-Yu-An <linyuan80226@gmail.com>
2026-04-07 17:54:07 +0200

    Adding to Data file basic data classes structure
    still need to fill up the blank for the rest of the data related classes

---
+++ MusicWasp/Data/SongData.cs
@@ -0,0 +1,17 @@
+﻿using MusicWasp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicWasp.Data
+{
+    public class SongData : DatabaseConnection
+    {
+        public int InsertSong(Song song)
+        {
+            string query = $"INSERT INTO SONG(song_id, title, duration_seconds, play_count) " 
+                + $"VALUES(NULL, " + $"{song.SongID}, '{song.Title}', {song.Duration}, {song.PlayCount});";
+            return this.Insert(query);
+        }
+    }
+}
